82a822f655b437e91cd11bb9477fbd6b480e6cd5
trinhxhai2000@gmail.com <trinhxhai@gmail.com>
2023-03-31 10:52:16 +0700

    upd

--- src/components/App.js
+++ src/components/App.js
@@ -10,62 +10,81 @@ import { useState } from "react";
 import theme from "./ui/Theme";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  console.log(theme);
+    console.log(theme);
 
-  const [value, setValue] = useState(0);
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+    const [value, setValue] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Header
-          value={value}
-          setValue={setValue}
-          selectedIndex={selectedIndex}
-          setSelectedIndex={setSelectedIndex}
-        />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <LandingPage
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/services"
-            element={
-              <Services
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/customsoftware"
-            element={
-              <CustomSoftware
-                setValue={setValue}
-                setSelectedIndex={setSelectedIndex}
-              />
-            }
-          />
-          <Route exact path="/mobileapps" element={<div>Mobile Apps</div>} />
-          <Route exact path="/websites" element={<div>Website</div>} />
-          <Route exact path="/revolution" element={<div>Revolution</div>} />
-          <Route exact path="/about" element={<div>About</div>} />
-          <Route exact path="/contact" element={<div>Contact</div>} />
-          <Route exact path="/estimate" element={<div>Estimate</div>} />
-        </Routes>
-        <Footer setValue={setValue} setSelectedIndex={setSelectedIndex} />
-      </BrowserRouter>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Header
+                    value={value}
+                    setValue={setValue}
+                    selectedIndex={selectedIndex}
+                    setSelectedIndex={setSelectedIndex}
+                />
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <LandingPage
+                                setValue={setValue}
+                                setSelectedIndex={setSelectedIndex}
+                            />
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/services"
+                        element={
+                            <Services
+                                setValue={setValue}
+                                setSelectedIndex={setSelectedIndex}
+                            />
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/customsoftware"
+                        element={
+                            <CustomSoftware
+                                setValue={setValue}
+                                setSelectedIndex={setSelectedIndex}
+                            />
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/mobileapps"
+                        element={<div>Mobile Apps</div>}
+                    />
+                    <Route
+                        exact
+                        path="/websites"
+                        element={<div>Website</div>}
+                    />
+                    <Route
+                        exact
+                        path="/revolution"
+                        element={<div>Revolution</div>}
+                    />
+                    <Route exact path="/about" element={<div>About</div>} />
+                    <Route exact path="/contact" element={<div>Contact</div>} />
+                    <Route
+                        exact
+                        path="/estimate"
+                        element={<div>Estimate</div>}
+                    />
+                </Routes>
+                <Footer
+                    setValue={setValue}
+                    setSelectedIndex={setSelectedIndex}
+                />
+            </BrowserRouter>
+        </ThemeProvider>
+    );
 }
 
 export default App;
